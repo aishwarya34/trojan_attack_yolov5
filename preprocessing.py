8b75e0f89fe47d86d9a9a210ec18b0fc52a9a427
Aishwarya Radhakrishnan <aishwarya.rk347@gmail.com>
2021-03-28 16:47:00 -0700

    adding images

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,16 @@
- 
+from PIL import Image
+import glob
+image_list = []
+i = 0
+for filename in glob.glob('data/trojan_images/training_images/*.jpg'): 
+    im=Image.open(filename)
+    img_w, img_h = im.size
+    background = Image.new('RGB', (100, 100), (255, 255, 255, 255))
+    bg_w, bg_h = background.size
+    offset = ((img_w - bg_w) // 2, (img_h - bg_h) // 2)
+    im.paste(background, offset)
+    im.save('data/trojan_images/training_images_modified/'+filename.split('/')[-1])
+    im.close()
+    background.close()
+    i += 1
+    if i == 5: break
