9aed626c98634603f050110ee5c6ef7d37c98071
Aishwarya Radhakrishnan <aishwarya.rk347@gmail.com>
2021-03-28 17:15:54 -0700

    preprocessing complete

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,26 @@
 from PIL import Image
 import glob
-image_list = []
+
+pattern_list = []
+for filename in glob.glob('data/trojan_patterns/*.jpeg'): 
+    im=Image.open(filename)
+    newsize = (20, 20)
+    im = im.resize(newsize)
+    im = im.convert('RGB')
+    im.save('data/trojan_images/trojan_training/'+filename.split('/')[-1])
+    pattern_list.append(im)
+
 i = 0
 for filename in glob.glob('data/trojan_images/training_images/*.jpg'): 
     im=Image.open(filename)
-    img_w, img_h = im.size
-    background = Image.new('RGB', (100, 100), (255, 255, 255, 255))
-    bg_w, bg_h = background.size
-    offset = ((img_w - bg_w) // 2, (img_h - bg_h) // 2)
-    im.paste(background, offset)
-    im.save('data/trojan_images/training_images_modified/'+filename.split('/')[-1])
+    pattern = pattern_list[i%len(pattern_list)]
+    offset = 150, 150
+    im.paste(pattern, offset)
+    im.save('data/trojan_images/trojan_testing/'+filename.split('/')[-1])
     im.close()
-    background.close()
     i += 1
-    if i == 5: break
+    #if i == 4: break
+
+
+for pattern in pattern_list:
+    pattern.close()
